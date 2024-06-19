8f92241053a9b578208d3bf1cd04b46974835e6e
Sven Vukelić <sven@vukelic.xyz>
2024-06-19 16:58:47 +0200

    Added Attack Form child row

---
+++ Deedforge/Deedforge.Client/Helpers/TableChildRowWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Deedforge.Client.Helpers
+{
+    public class TableChildRowWrapper<T>
+    {
+        public bool ShowDetails { get; set; } = false;
+        public T Item { get; set; }
+
+        public TableChildRowWrapper(T item)
+        {
+            Item = item;
+        }
+
+        public void ToggleDetails()
+        {
+            ShowDetails = !ShowDetails;
+        }
+    }
+}
