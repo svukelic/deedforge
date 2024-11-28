221349385f4f8b8fa5d2a0bea9ff45268732fddc
Sven Vukelić <sven@vukelic.xyz>
2024-11-28 09:19:26 +0100

    Formatting, Inventory and Weapons pages

--- Deedforge.Shared/Helpers/StringHelpers.cs
+++ Deedforge.Shared/Helpers/StringHelpers.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Deedforge.Shared.Helpers
+﻿namespace Deedforge.Shared.Helpers
 {
     public static class StringHelpers
     {
-        
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = field?.GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute), false)
+                .FirstOrDefault() as System.ComponentModel.DescriptionAttribute;
+            return attribute?.Description ?? value.ToString();
+        }
     }
 }
